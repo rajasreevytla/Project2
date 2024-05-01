6164a38de981dbae164e5e2e9e26a0061cdfa5eb
rajasreevytla <147678823+rajasreevytla@users.noreply.github.com>
2024-05-01 02:23:03 -0400

    Update Show.py

--- Show.py
+++ Show.py
@@ -1,8 +1,7 @@
-# Show.py
 from Media import Media
 
 class Show(Media):
-    def __init__(self, media_id, show_type, title, directors, actors, average_rating, country_code, date_added, release_year, rating, duration, genres, description):
+    def __init__(self,media_id,show_type,title, average_rating, directors, actors, country_code, date_added, release_year, rating, duration, genres, description):
         super().__init__(media_id, title, average_rating)
         self._show_type = show_type
         self._directors = directors
@@ -14,11 +13,11 @@ class Show(Media):
         self._duration = duration
         self._genres = genres
         self._description = description
-
-    # Accessors
+    def items(self,media_id):
+        self._media_id = media_id
+    
     def get_show_type(self):
         return self._show_type
-
     def get_directors(self):
         return self._directors
 
@@ -45,8 +44,6 @@ class Show(Media):
 
     def get_description(self):
         return self._description
-
-    # Mutators
     def set_show_type(self, show_type):
         self._show_type = show_type
 
@@ -76,3 +73,4 @@ class Show(Media):
 
     def set_description(self, description):
         self._description = description
+    
